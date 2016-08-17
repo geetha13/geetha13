bb999c4dac7a9f94446b0ce13b2124e86fa86c58
geetha13 <j.geethakumar92@gmail.com>
2016-08-17 20:05:17 +0530

    ologn1.c

---
+++ ologn1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+void print(int arr[],int size)
+{
+printf("repaeting elements are");
+for(i=0;i<size;i++)
+for(j=j+1;j<size;j++)
+if(arr[i]=arr[j])
+printf*"%D",arr[i])
+printf("%d",arr[i])
+}
+int main()
+{
+int arr[]={4,1,4,6,3,1}
+int arr_size=sizeof(arr)
+printf(arr,arr_size);
+getch();
+return 0;
+}
