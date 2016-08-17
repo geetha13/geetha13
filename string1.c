2e83e06286d592fd860c1786e2e5ffdac273e308
geetha13 <j.geethakumar92@gmail.com>
2016-08-17 21:08:46 +0530

    string1.c

---
+++ string1.c
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void build lowestnumberrec(string str,int n,string & res)
+{
+if(n==0)
+{
+res.append(str);
+return;
+}
+int len=str.len();
+if(len<=n)
+return;
+int minindex=0;
+for(int i=1;i<n;i++)if(str[i]<str[minindex]);
+string new_str=str.substr(minlength+1);
+{
+string res=" ";
+return res;
+}
+int main()
+{
+string str="121198";
+int n=2;
+count<<buildlowestnumbers(str,n);
+return 0;
+}
